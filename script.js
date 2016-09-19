0cacc8fa9b6f7c116bd7e4ccdb8bb537ded6a16e
Kyle Marx <Kyle Marx>
2016-09-18 21:32:18 -0700

    added bootstrap formatting to form elements and buttons, initiated work in script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
  * student_array - global array to hold student objects
  * @type {Array}
  */
-
+var student_array = [];
 /**
  * inputIds - id's of the elements that are used to add students
  * @type {string[]}
@@ -14,16 +14,33 @@
 /**
  * addClicked - Event Handler when user clicks the add button
  */
+function addClicked() {
+    console.log('add clicked');
+    //when clicked the add clicked should create a student object
+
+    //then add the student object to the
+}
 
 /**
  * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
  */
+function cancelClicked() {
+    console.log('cancel clicked');
+}
 
 /**
  * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
  *
  * @return undefined
  */
+function addStudent() {
+    var studentInfo = {
+        studentName: $('#studentName').val(),
+        course: $('#course').val(),
+        studentGrade: $('#studentGrade').val()
+    };
+    student_array.push(studentInfo);
+}
 
 /**
  * clearAddStudentForm - clears out the form values based on inputIds variable
@@ -51,8 +68,17 @@
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
  */
+function reset(){
+    console.log('document loaded','page reset');
+    //add click handlers for buttons
+    $('button.btn-success').click(addClicked);
+    $('button.btn-default').click(cancelClicked);
+}
 
 
 /**
  * Listen for the document to load and reset the data to the initial state
  */
+$(document).ready(function() {
+    reset();
+});
