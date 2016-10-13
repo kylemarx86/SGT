a77c3b9550e1a495ee0033fa075aa909d80b50a1
Kyle Marx <Kyle Marx>
2016-10-12 19:26:36 -0700

    added connection files

--- read.php
+++ read.php
@@ -1,4 +1,8 @@
 <?php
 date_default_timezone_set('America/Los_Angeles');
+
+require_once("mysql_connect.php");
+$query = "";
+
 print(json_encode(['success' => true,'data' =>[['id' => 1,'name' => 'Elizabeth','grade' => 89,'course' => 'Library Science']]]));
 ?>
