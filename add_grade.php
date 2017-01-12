ea2dca59495cb72dabce74b3c23336299b5df258
Kyle Marx <Kyle Marx>
2017-01-11 23:32:49 -0800

    reworked selection queries in add_grade.php

--- add_grade.php
+++ add_grade.php
@@ -2,12 +2,12 @@
 date_default_timezone_set('America/Los_Angeles');
 require_once('mysql_connect.php');
 
-//$course = "'wrastling 402'";
-//$student = "'Stan Marsh'";
-//$grade = '60';
-$student = $_POST['name'];
-$course = $_POST['course'];
-$grade = $_POST['grade'];
+$course = "wrastling 402";
+$student = "Stan Marsh";
+$grade = 60;
+//$student = $_POST['name'];
+//$course = $_POST['course'];
+//$grade = $_POST['grade'];
 
 //theory check to see if there is a grade already for a student in a specific class
     //if so return an error
@@ -19,7 +19,7 @@ $data['success'] = false;
 $data['errors'] = [];
 
 //get the id of the course
-$course_query = "SELECT ID FROM `courses` WHERE name='".$course."'";
+$course_query = "SELECT ID FROM `courses` WHERE name='$course'";
 $course_result = mysqli_query($conn, $course_query);
 
 if(mysqli_num_rows($course_result)){
@@ -30,7 +30,7 @@ if(mysqli_num_rows($course_result)){
 }else{
     //insert the course into the db and retrieve the id
     //    echo 'we need to add this class '.$course;    //temp while testing
-    $add_course_query = "INSERT INTO `courses` SET name = '".$course."'";
+    $add_course_query = "INSERT INTO `courses` SET name = '$course'";
     $add_course_result = mysqli_query($conn, $add_course_query);
 
 //    print("<br>".mysqli_insert_id($conn));    //temp while testing
@@ -39,7 +39,7 @@ if(mysqli_num_rows($course_result)){
 }
 
 //get the id of the student
-$student_query = "SELECT ID FROM `students` WHERE name = '".$student."'";
+$student_query = "SELECT ID FROM `students` WHERE name = '$student'";
 $student_result = mysqli_query($conn, $student_query);
 
 if(mysqli_num_rows($course_result)){
@@ -49,7 +49,7 @@ if(mysqli_num_rows($course_result)){
     }
 }else{
     //insert the student into the db and retrieve the id
-    $add_student_query = "INSERT INTO `students` SET name = '".$student."'";
+    $add_student_query = "INSERT INTO `students` SET name = '$student'";
     $add_student_result = mysqli_query($conn, $add_student_query);
 
     $student_id = mysqli_insert_id($conn);
@@ -60,7 +60,7 @@ if(mysqli_num_rows($course_result)){
     //need to further investigate the mysqli_insert_id
 if(0 < $grade && $grade <= 100){
     //if all other parts are successful write the addition to the grades table
-    $add_grade_query = "INSERT INTO `grades` SET student_id = ".$student_id.", course_id = ".$course_id.", grade = ".$grade.", created = NOW()";
+    $add_grade_query = "INSERT INTO `grades` SET student_id = '$student_id', course_id = '$course_id', grade = $grade, created = NOW()";
     $add_grade_result = mysqli_query($conn, $add_grade_query);
 
     if(mysqli_num_rows($add_grade_result)){
